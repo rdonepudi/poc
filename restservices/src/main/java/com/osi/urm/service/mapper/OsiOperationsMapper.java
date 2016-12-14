b4db8c71ef06026cb360f0c90dfdc4e255d99203
Sanket Nehe <snehe@osius.com>
2016-12-14 17:25:52 +0530

    OsiOperation mapper

---
+++ restservices/src/main/java/com/osi/urm/service/mapper/OsiOperationsMapper.java
@@ -0,0 +1,24 @@
+package com.osi.urm.service.mapper;
+
+import java.util.List;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+import com.osi.urm.domain.OsiFunctions;
+import com.osi.urm.domain.OsiOperations;
+import com.osi.urm.service.dto.OsiOperationsDTO;
+
+@Mapper(componentModel="spring", unmappedTargetPolicy = ReportingPolicy.IGNORE, uses={OsiFunctions.class})
+public interface OsiOperationsMapper {
+
+	OsiOperationsDTO osiOperationsToOsiOperationsDTO(OsiOperations osiOperations);
+	
+	List<OsiOperationsDTO> osiOperationsListToOsiOperationsDTOList(List<OsiOperations> osiOperations);
+	
+	OsiOperations osiOperationsDTOToOsiOperations(OsiOperationsDTO osiOperationsDTO);
+	
+	List<OsiOperations> osiOperationsDTOListToOsiOperationsList(List<OsiOperationsDTO> osiOperationsDTO);
+
+	
+}
