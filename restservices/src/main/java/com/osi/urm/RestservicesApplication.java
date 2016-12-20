79dc3f83c9c4e9766324cd814d2b8f8036cbc9d2
Pradeep Rawate <prawate@nisum.com>
2016-12-20 18:55:25 +0530

    Reverted Code back...

--- restservices/src/main/java/com/osi/urm/RestservicesApplication.java
+++ restservices/src/main/java/com/osi/urm/RestservicesApplication.java
@@ -2,10 +2,8 @@ package com.osi.urm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 public class RestservicesApplication {
 
 	public static void main(String[] args) {
